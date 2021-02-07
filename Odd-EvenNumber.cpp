47b9321357abf2fbb28e274c307a5d7695c94d61
Bockidd <78609133+Bockidd@users.noreply.github.com>
2021-02-07 16:28:07 +0300

    Update Odd-EvenNumber.cpp

--- Odd-EvenNumber.cpp
+++ Odd-EvenNumber.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 using namespace std;
-int main()
+int main ()
 {
-	int sayi;
-	int cift=0;
-	int tek=0;
-	int toplamcift=0;
-	int toplamtek = 0;
-	cout << "10 sayi giriniz: " << endl;
-	for (int i = 0; i < 10; i++)
-	{
-		cin >> sayi;
-		if (sayi % 2 == 0) {
+int number;
+int even = 0;
+int odd = 0;
+int sumeven = 0;
+int sumodd = 0;
+cout << "Enter 10 numbers:" << endl;
+for (int i = 0; i <10; i ++)
+{
+cin >> number;
+if (number% 2 == 0) {
 
-			cift = cift + 1;		
-			toplamcift = toplamcift + sayi;
-			
-		}
-		else {
-			tek = tek + 1;			
-			toplamtek = toplamtek + sayi;
-			
-		}
-	}
-	cout << "Girilen çift sayı sayısı: " << cift << endl;
-	cout << "Girilen tek sayı sayısı: " << tek << endl;
+even = even + 1;
+sumeven = sumeven + number;
 
-	cout << "Çift sayıların toplamı: " << toplamcift << endl;
-	cout << "Tek sayıların toplamı: " << toplamtek << endl;
+}
+else {
+odd = odd + 1;
+sumodd = sumodd + number;
 
-	
+}
+}
+cout << "Number of even numbers entered:" << even << endl;
+cout << "Number of odd numbers entered:" << odd << endl;
+
+cout << "Sum of even numbers:" << sumeven << endl;
+cout << "Sum of odd numbers:" << sumodd << endl;
+
+
 }
