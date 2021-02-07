344bf586eae23bfb1af86c955e5cb294d568dc34
Bockidd <78609133+Bockidd@users.noreply.github.com>
2021-02-07 16:24:45 +0300

    Update DiceProgram.cpp

--- DiceProgram.cpp
+++ DiceProgram.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 #include <time.h> 
 // dice game
+
 int main () (
+ 
+int random;
+int start;
 
-  int random;
-  int start;
+cout << "Welcome to the dice roll program!" << endl;
+cout << "Press 1 to roll the dice:";
 
-  cout << "Welcome to the dice roll program!" << endl;
-  cout << "Press 1 to roll the dice:";
+cin >> start;
 
-  cin >> start;
-
-
-
-  if (start == 1) (
+if (start == 1) {
 
 srand (time (0));
 
 random = rand () 6% + 1;
-
+//Randomly pick one of 6 numbers
+    
 cout << "Dice 1:" << random << endl;
 
 
