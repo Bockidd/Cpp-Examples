aaba7ca9a2a90c119ca580175215da44b19e7ee7
Bockidd <78609133+Bockidd@users.noreply.github.com>
2021-02-07 17:30:21 +0300

    Create colorgame.cpp
    You enter color and the text becomes that color

---
+++ colorgame.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	string color;
+	cout << "Welcome to the color game!" << endl;
+	cout << "Enter the color you want (Blue,green,red,purple,yellow or grey): " << endl;
+	cin >> color;
+
+	if (color == "blue") {
+
+		system("color 1");
+		//color changing process
+		cout << "Everything is blue!!" << endl;
+	}
+	else if (color == "green") {
+
+		system("color 2");
+		cout << "Everything is green!!" << endl;
+	}
+
+	else if (color == "red") {
+
+		system("color 4");
+		cout << "Everything is red!!" << endl;
+	}
+	else if (color == "purple") {
+
+		system("color 5");
+		cout << "Everything is purple!!" << endl;
+	}
+	else if (color == "yellow") {
+
+		system("color 6");
+		cout << "Everything is yellow!!" << endl;
+	}
+	
+	else if (color == "grey") {
+
+		system("color 8");
+		cout << "Everything is grey!!" << endl;
+	}
+
+	
+	
+}
