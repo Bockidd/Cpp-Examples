6ee118b71643168b1e3b3fab617ddab2a6a477b3
Bockidd <78609133+Bockidd@users.noreply.github.com>
2021-02-10 18:50:23 +0300

    Create backgroundcolor.cpp
    Change the background and color of codes

---
+++ backgroundcolor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	 //Change the background and color of codes
+
+
+	/*   system ("color XY");
+	
+	  X          Y              
+	  1  Blue	   9	Light Blue
+		2  Green	 0	Black
+		3  Aqua	   A	Light Green
+		4	 Red	   B	Light Aqua
+		5	 Purple	 C	Light Red
+		6	 Yellow	 D	Light Purple
+		7  White	 E	Light Yellow
+		8	 Gray	   F	Bright White 
+    
+    Select X and Y here
+
+	  example:                 */
+
+     
+	system("color 59");
+	cout << "Hello World" << endl;
+
+}
