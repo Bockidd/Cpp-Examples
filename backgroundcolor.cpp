cf10b6334121f5313f7295718ff0f7ca899c503f
Bockidd <78609133+Bockidd@users.noreply.github.com>
2021-02-10 18:52:20 +0300

    Update backgroundcolor.cpp

--- backgroundcolor.cpp
+++ backgroundcolor.cpp
@@ -8,17 +8,17 @@ int main() {
 
 	/*   system ("color XY");
 	
-	  X          Y              
-	  1  Blue	   9	Light Blue
-		2  Green	 0	Black
-		3  Aqua	   A	Light Green
-		4	 Red	   B	Light Aqua
-		5	 Purple	 C	Light Red
-		6	 Yellow	 D	Light Purple
-		7  White	 E	Light Yellow
-		8	 Gray	   F	Bright White 
+	  X                Y              
+	  1   Blue	   9	Light Blue
+	  2   Green	   0	Black
+	  3   Aqua	   A	Light Green
+	  4   Red	   B	Light Aqua
+	  5   Purple	   C	Light Red
+	  6   Yellow	   D	Light Purple
+	  7   White	   E	Light Yellow
+	  8   Gray	   F	Bright White 
     
-    Select X and Y here
+          Select X and Y here
 
 	  example:                 */
 
