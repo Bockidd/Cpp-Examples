dd076bfa06915e370ca8f081eb19b518d4edcb6b
Bockidd <78609133+Bockidd@users.noreply.github.com>
2021-02-16 13:02:38 +0300

    Update Bodymassindex.cpp

--- Bodymassindex.cpp
+++ Bodymassindex.cpp
@@ -14,22 +14,22 @@ cout << "Please enter your height (exp: 1.55,1.85):";
 cin >> height;
 
 cout << "Enter your weight (eg 48.55):";
-gin >> weight;
+cin >> weight;
 
 index = weight / (height * height);
 
-if (index <18) (
+if (index <18) {
 
 cout << "Your weight is below ideal weight";
 }
 
 
-else if (index <24) (
+else if (index <24) {
 
 cout << "Your weight is ideal.";
 }
 
-else if (index <34) (
+else if (index <34) {
 
 cout << "Your weight is above ideal weight";
 }
